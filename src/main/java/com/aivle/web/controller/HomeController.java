7567b017495eea6d0906bb9e11cb3374b937117c
hongseok <wjsghdtjr729@gmail.com>
2025-01-15 13:33:12 +0900

    sidebar 서브메뉴 추가, 홈버튼 -> 홈 서브메뉴 클릭스 index2 파일로 이동

--- src/main/java/com/aivle/web/controller/HomeController.java
+++ src/main/java/com/aivle/web/controller/HomeController.java
@@ -2,13 +2,19 @@ package com.aivle.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
         return "index"; // index.html을 반환합니다.
+    }
+
+    @GetMapping("/index2")
+    public String index2() {
+        return "index2"; // templates/index2.html 반환
     }
 
 }
